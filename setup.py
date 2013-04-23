7e014267ebe546793eec7c65dc859f9740c241d9
Diane Trout <diane@caltech.edu>
2013-04-23 15:42:51 -0700

    Add a name, include the python code.
    Its not a very good name.

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 setup(
+    name = "swalign",
+    description = "cython based swaligner setup to work with python3",
+    version = '0.1',
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension('swalign.cswalign',
                              ['swalign/cswalign.pyx'])],
+    packages = ["swalign"],
     requires = ['cython (>=0.18)'],
 )
                              
